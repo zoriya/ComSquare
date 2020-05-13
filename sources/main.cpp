50949b5eb43546d97e9ace99ffb8a9d19393cbaa
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-05-13 18:38:06 +0200

    resizing the window

--- sources/main.cpp
+++ sources/main.cpp
@@ -76,7 +76,7 @@ int main(int argc, char **argv)
 	}
 	QApplication app(argc, argv);
 	QApplication::setAttribute(Qt::AA_DisableWindowContextHelpButton);
-	Renderer::QtSFML renderer(1080, 1920);
+	Renderer::QtSFML renderer(1100, 1100);
 	try {
 		SNES snes(argv[1], renderer);
 		renderer.createWindow(snes, 60);
