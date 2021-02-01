be689f2cdab115694442e56f21b4c523cbc888ed
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-02-01 15:24:38 +0100

    adding doc for PPUUtils

--- sources/PPU/PPUUtils.hpp
+++ sources/PPU/PPUUtils.hpp
@@ -9,13 +9,20 @@
 
 namespace ComSquare::PPU
 {
+	//! @brief Used to parse easily VRAM Tile information
 	union TileMapData {
 		struct {
+			//! @brief Tile X offset
 			uint16_t posX: 4;
+			//! @brief Tile Y offset
 			uint16_t posY: 6;
+			//! @brief Palette number used by the Tile
 			uint16_t palette: 3;
+			//! @brief True if the Tile has priority
 			uint16_t tilePriority: 1;
+			//! @brief True if the Tile need to be horizontally flipped
 			uint16_t horizontalFlip: 1;
+			//! @brief True if the Tile need to be vertically flipped
 			uint16_t verticalFlip: 1;
 		};
 		uint16_t raw = 0;
