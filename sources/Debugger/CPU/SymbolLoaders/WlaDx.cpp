f33593c51f8d1b3afe5ba45266956c5cdae84f31
Zoe Roux <zoe.roux@sdg.moe>
2021-02-17 11:30:16 +0100

    Starting the symbol reader

--- sources/Debugger/CPU/SymbolLoaders/WlaDx.cpp
+++ sources/Debugger/CPU/SymbolLoaders/WlaDx.cpp
@@ -2,6 +2,7 @@
 // Created by Zoe Roux on 2/6/21.
 //
 
+#include <regex>
 #include "WlaDx.hpp"
 
 namespace ComSquare::Debugger
@@ -9,8 +10,15 @@ namespace ComSquare::Debugger
 	std::vector<Label> WlaDx::parse(std::fstream symbolFile)
 	{
 		std::vector<Label> labels;
+		std::string line;
 
+		while (std::getline(symbolFile, line)) {
+			line = line.substr(0, line.rfind(';'));
+			std::smatch match;
+			std::regex re("\\[\\S+\\]");
 
+			if (!std::regex_search(line, match, re)
+		}
 		return labels;
 	}
 }
