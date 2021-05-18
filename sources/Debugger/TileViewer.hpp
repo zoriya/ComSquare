e5ce5efb25de4754e4d22ece201636d743e1a311
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-05-19 00:41:18 +0200

    rm comment

--- sources/Debugger/TileViewer.hpp
+++ sources/Debugger/TileViewer.hpp
@@ -19,35 +19,6 @@ namespace ComSquare::Renderer
 #include "../PPU/PPU.hpp"
 #include "../../ui/ui_tileView.h"
 
-/*
-//! @brief The qt model that bind the logs to the view.
-class CGramModel : public QAbstractTableModel
-{
-Q_OBJECT
-private:
-	//! @brief The ppu to log the cgram.
-	ComSquare::PPU::PPU &_ppu;
-public:
-	//! @brief The number of columns
-	const int column = 16;
-	//! @brief The number of rows
-	const int rows = 16;
-	int x;
-	int y;
-	explicit CGramModel(ComSquare::PPU::PPU &ppu);
-	CGramModel(const CGramModel &) = delete;
-	const CGramModel &operator=(const CGramModel &) = delete;
-	~CGramModel() override = default;
-
-	//! @brief The number of row the table has.
-	int rowCount(const QModelIndex &parent) const override;
-	//! @brief The number of column the table has.
-	int columnCount(const QModelIndex &parent) const override;
-	//! @brief Return a data representing the table cell.
-	QVariant data(const QModelIndex &index, int role) const override;
-};
- */
-
 namespace ComSquare::Debugger
 {
 	//! @brief window that allow the user to view all data going through the memory bus.
