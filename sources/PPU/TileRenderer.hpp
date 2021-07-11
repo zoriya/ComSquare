8a84258c5a2c97c11d18eef253c2867fe03065a4
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-07-11 19:15:46 +0200

    adding tests for the getPalette function

--- sources/PPU/TileRenderer.hpp
+++ sources/PPU/TileRenderer.hpp
@@ -46,7 +46,9 @@ namespace ComSquare::PPU
 		//! @return The color Reference
 		uint8_t getPixelReferenceFromTileRow(uint16_t tileRowAddress, uint8_t pixelIndex);
 		//! @brief Gives the actual selected palette with all of it's colors
+		//! @param nbPalette The index of the palette wanted
 		//! @return The array of color of the palette
+		//! @note The return and argument depends on the current bpp
 		//! @warning Values are CGRAM colors use PPU::getRealColor function to get the actual real color
 		std::vector<uint16_t> getPalette(int nbPalette);
 		//! @brief read the 2bpp value for a pixel (used multple times for 4bpp and 8bpp)
